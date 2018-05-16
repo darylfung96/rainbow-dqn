6eec66d370bdbfa0a434484c321293850ce87e45
Daryl Fung <darylfung96@gmail.com>
2018-05-15 23:52:46 -0500

    added action return from agent

--- distributional_dqn.py
+++ distributional_dqn.py
@@ -9,28 +9,31 @@ class DistributionalDQN(nn.Module):
         super(DistributionalDQN, self).__init__()
 
         self.batch_size = batch_size
+        self.action_size = action_size
 
-        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size[0])
         self.batchnorm1 = nn.BatchNorm2d(32)
-        self.conv1_output_size = input_size - kernel_size[0] + 2 + 1
-        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv1_output_size = input_size - kernel_size[0] + 0 + 1
+        self.conv2 = nn.Conv2d(32, 64, kernel_size[1])
         self.batchnorm2 = nn.BatchNorm2d(64)
-        self.conv2_output_size = self.conv1_output_size - kernel_size[1] + 2 + 1
-        self.conv3 = nn.Conv2d(64, 32, 5)
+        self.conv2_output_size = self.conv1_output_size - kernel_size[1] + 0 + 1
+        self.conv3 = nn.Conv2d(64, 32, kernel_size[2])
         self.batchnorm3 = nn.BatchNorm2d(32)
-        self.conv3_output_size = self.conv2_output_size - kernel_size[2] + 2 + 1
+        self.conv3_output_size = self.conv2_output_size - kernel_size[2] + 0 + 1
 
-        self.fc1 = nn.Linear(self.conv3_output_size, 64)
-        self.fc2 = nn.Linear(64, action_size) # add distribution list
-
-        # self.policy_output = nn.Linear()
+        self.fc1 = nn.Linear(self.conv3_output_size ** 2 * self.batch_size * 32, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.policy_output = nn.Linear(64, action_size) # add distributional list
 
     def forward(self, x):
         conv1_output = F.relu(self.batchnorm1(self.conv1(x)))
-        conv2_output = F.relu(self.batchnorm2(self.conv2(x)))
-        conv3_output = F.relu(self.batchnorm3(self.conv3(x)))
+        conv2_output = F.relu(self.batchnorm2(self.conv2(conv1_output)))
+        conv3_output = F.relu(self.batchnorm3(self.conv3(conv2_output)))
 
         convolution_output = conv3_output.view(self.batch_size, -1)
         fc1_output = F.relu(self.fc1(convolution_output))
         fc2_output = F.relu(self.fc2(fc1_output))
+        policy_output = F.softmax(self.policy_output(fc2_output))
+
+        return policy_output.data.numpy()
 
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import gym
 from scipy.misc import imresize
 from PIL import Image
+from torch.autograd import Variable
+import torch
+import numpy as np
+
+from distributional_dqn import DistributionalDQN
 
 env = gym.make('CartPole-v0')
 env.reset()
 
+agent = DistributionalDQN(1, env.action_space.n, 80, [5, 3, 5])
+
 for _ in range(1000):
     rendered = env.render(mode='rgb_array')
     resized = imresize(rendered, [80, 80, 30])
-    img = Image.fromarray(resized, 'RGB').convert('LA')
-    env.step(env.action_space.sample())
+    resized = np.reshape(resized, [1, 80, 80, 3]).astype(np.float32)
+    resized = resized.transpose((0, 3, 2, 1))
+
+    variable_input = Variable(torch.from_numpy(resized))
+
+    # img = Image.fromarray(resized, 'RGB').convert('LA')
+    action = agent(variable_input)
+    best_action = np.argmax(action)
+    env.step(best_action)
