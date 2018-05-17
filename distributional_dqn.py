5538fb64e05cd43d431fac8c895554d8421ddc18
Daryl Fung <darylfung96@gmail.com>
2018-05-17 10:41:29 -0500

    added select actions and distributional actions

--- distributional_dqn.py
+++ distributional_dqn.py
@@ -1,15 +1,16 @@
-import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class DistributionalDQN(nn.Module):
 
-    def __init__(self, batch_size, action_size, input_size, kernel_size: list):
+    def __init__(self, batch_size, action_size, atom_size, input_size, kernel_size: list):
         super(DistributionalDQN, self).__init__()
 
         self.batch_size = batch_size
         self.action_size = action_size
+        self.atom_size = atom_size
 
         self.conv1 = nn.Conv2d(3, 32, kernel_size[0])
         self.batchnorm1 = nn.BatchNorm2d(32)
@@ -23,7 +24,11 @@ class DistributionalDQN(nn.Module):
 
         self.fc1 = nn.Linear(self.conv3_output_size ** 2 * self.batch_size * 32, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.policy_output = nn.Linear(64, action_size) # add distributional list
+
+        self.policy_distribution_output = []
+
+        for _ in range(action_size):
+            self.policy_distribution_output.append(nn.Linear(64, atom_size))
 
     def forward(self, x):
         conv1_output = F.relu(self.batchnorm1(self.conv1(x)))
@@ -33,7 +38,10 @@ class DistributionalDQN(nn.Module):
         convolution_output = conv3_output.view(self.batch_size, -1)
         fc1_output = F.relu(self.fc1(convolution_output))
         fc2_output = F.relu(self.fc2(fc1_output))
-        policy_output = F.softmax(self.policy_output(fc2_output))
 
-        return policy_output.data.numpy()
+        policy_output = np.array([]).reshape(0, self.atom_size)
+        for policy_distribution_output in self.policy_distribution_output:
+            policy_output = np.vstack([policy_output, F.softmax(policy_distribution_output(fc2_output)).data.numpy()])
+
+        return policy_output
 
--- main.py
+++ main.py
@@ -7,20 +7,38 @@ import numpy as np
 
 from distributional_dqn import DistributionalDQN
 
+IMAGE_SIZE = 80
+ATOM_SIZE = 51
+V_MIN = -10
+V_MAX = 10
+z = np.linspace(V_MIN, V_MAX, ATOM_SIZE)
+
 env = gym.make('CartPole-v0')
 env.reset()
 
-agent = DistributionalDQN(1, env.action_space.n, 80, [5, 3, 5])
+agent = DistributionalDQN(1, env.action_space.n, ATOM_SIZE, IMAGE_SIZE, [5, 3, 5])
+
+
+def select_action(z, probs):
+    z_probs = np.multiply(probs, z)
+    best_action = np.sum(z_probs, axis=1).argmax()
+    return best_action
+
 
 for _ in range(1000):
     rendered = env.render(mode='rgb_array')
-    resized = imresize(rendered, [80, 80, 30])
+    resized = imresize(rendered, [IMAGE_SIZE, IMAGE_SIZE])
+    # img = Image.fromarray(resized, 'RGB').convert('LA')
     resized = np.reshape(resized, [1, 80, 80, 3]).astype(np.float32)
     resized = resized.transpose((0, 3, 2, 1))
 
     variable_input = Variable(torch.from_numpy(resized))
 
-    # img = Image.fromarray(resized, 'RGB').convert('LA')
-    action = agent(variable_input)
-    best_action = np.argmax(action)
-    env.step(best_action)
+    probs = agent(variable_input)
+    best_action = select_action(z, probs)
+    observation, reward, done, _ = env.step(best_action)
+
+    if done:
+        env.reset()
+
+
