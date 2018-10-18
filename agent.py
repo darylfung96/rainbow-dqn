fix reward error (negative sign)

agent.py
@@ -31,7 +31,7 @@ class Agent:
         self.target_brain = DistributionalDQN(action_size=action_size, atom_size=atom_size,
                                        input_size=input_size, kernel_size=kernel_size)
         self.target_brain.load_state_dict(self.brain.state_dict())
-        self.optim = optim.Adam(self.brain.parameters(), lr=0.01)
+        self.optim = optim.Adam(self.brain.parameters(), lr=0.001)
 
     def step(self, state_input):
         probs = self.brain(state_input)
@@ -131,7 +131,7 @@ class Agent:
             # loss = torch.sum(target_z_prob * torch.log(output_prob))
 
             loss = F.mse_loss(max_current_q, target)
-            total_loss = loss if total_loss is None else total_loss + loss
+            total_loss = -loss if total_loss is None else total_loss - loss
 
             # update td
             td = self.calculate_td(state_input, best_action, reward, done, next_state_input)

distributional_dqn.py
@@ -44,7 +44,7 @@ class DistributionalDQN(nn.Module):
         fc1_output = F.relu(self.fc1(x))
         fc2_output = F.relu(self.fc2(fc1_output))
         fc3_output = self.fc3(fc2_output)
-        # fc3_output = F.softmax(self.fc3(fc2_output))
+        # fc3_output = F.softmax(fc3_output)
         # policy_output = []
         # variable_policy_output = None
         # for policy_distribution_output in self.policy_distribution_output:

main.py
@@ -39,7 +39,7 @@ for current_episode in range(MAX_EPISODE):
     variable_input = Variable(torch.from_numpy(variable_input).float())
     best_action = agent.step(variable_input)
     next_input, reward, done, _ = env.step(best_action)
-    reward = reward if not done else -100
+    # reward = reward if not done else -100
     total_reward += reward
     next_input = np.reshape(next_input, [1, -1])
     next_variable_input = Variable(torch.from_numpy(next_input).float())